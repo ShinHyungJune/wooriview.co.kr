fcc85aa8fa9aa70e35cdb7c644f2654f8bff7355
ShinHyungJune <ssa4141@naver.com>
2023-05-23 14:22:23 +0900

    fix

--- pages/socialLogin.vue
+++ pages/socialLogin.vue
@@ -12,48 +12,25 @@ export default {
     },
 
     mounted() {
-        let self = this;
-        let push_token = localStorage.getItem("push_token");
-
         this.$axios.get("/api/pushTokens")
             .then(response => {
-                alert(response.data);
+                let pushToken = response.data;
+
+                this.$store.commit("setPushToken", pushToken);
+
+                this.$auth.loginWith('laravelSanctum', {
+                    data: {
+                        token: this.token,
+                        push_token : pushToken
+                    }
+                }).then(response => {
+
+                }).catch((e) => {
+                    alert("소셜로그인에 실패하였습니다.");
+
+                    return this.$router.push("/");
+                })
             })
-        alert(this.$store.state.push_token);
-        alert(push_token);
-
-        // this.$store.commit("init");
-        window.receiveFcmToken = (token) => {
-            alert(token);
-            console.log('Received FCM token:', token);
-            self.$store.commit("setPushToken", token);
-
-            self.$auth.loginWith('laravelSanctum', {
-                data: {
-                    token:self.token,
-                    push_token : self.$store.state.push_token
-                }
-            }).then(response => {
-
-            }).catch((e) => {
-                alert("소셜로그인에 실패하였습니다.");
-
-                return self.$router.push("/");
-            })
-
-        };
-
-        alert(JSON.stringify(window.AndroidBridge));
-
-        if(window.AndroidBridge) {
-            window.AndroidBridge.getFcmToken("fcm");
-        }
-
-        if(window.webkit) {
-            window.webkit.messageHandlers.getFcmToken.postMessage("fcm");
-        }
-
-
     }
 }
 </script>
