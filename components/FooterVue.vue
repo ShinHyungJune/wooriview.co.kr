ad63c70b5b13aa21d452a88885604707fe586c08
ShinHyungJune <ssa4141@naver.com>
2023-05-25 15:37:23 +0900

    fix sell number

--- components/FooterVue.vue
+++ components/FooterVue.vue
@@ -21,7 +21,7 @@
                     <nuxt-link to="/qnas">고객센터</nuxt-link>
                 </div>
                 <p class="Address">우리뷰(주) | 대표이사 : 정원철 </p>
-                <p class="Address">사업자등록번호 : 706-88-02900 | 통신판매업신고번호 제 2023-의정부흥선-0370호 </p>
+                <p class="Address">사업자등록번호 : 706-88-02900 | 통신판매업신고번호 : 제 2023-의정부흥선-0370호 </p>
                 <p class="Address">주소 : 서울특별시 노원구 상계동169-210 4층</p>
                 <p class="Address">메일 : admin@wooriview.co.kr | 전화 : 1644-2515 </p>
                 <p class="COPYRIGHT">COPYRIGHT 2022 우리뷰 ALL RIGHT RESERVED.</p>
