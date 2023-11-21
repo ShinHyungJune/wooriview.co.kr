9a06534a800d60ebb6436a6b8f95c49b7da12ef8
ShinHyungJune <ssa4141@naver.com>
2023-11-21 10:47:25 +0900

    fix

--- pages/admin/guides/create.vue
+++ pages/admin/guides/create.vue
@@ -17,7 +17,7 @@
                                 <p>이미지</p>
                             </div>
                             <div class="user">
-                                <input-images id="pc" :multiple="false" @change="(data) => form.files = data" :default="[item ? item.pc : '']" />
+                                <input-images id="pc" :multiple="false" @change="(data) => form.files = data" :default="[item && item.img ? item.img : '']" />
                                 <error :form="form" name="files" />
                             </div>
                         </li>
