e7316db4bbd4c1d31e092d89851d8b738c26dd7b
ShinHyungJune <ssa4141@naver.com>
2023-12-02 18:40:48 +0900

    fix

--- components/InputEditor.vue
+++ components/InputEditor.vue
@@ -87,7 +87,6 @@ export default {
                 }
             },
             theme: 'snow',
-            formats: ['link'],
             attributes: {
                 a: {
                     target: '_self',
