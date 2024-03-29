096f4ba5973a68c7d8044c49a6eace95c777c6f4
wooriview0 <admin@wooriview.co.kr>
2024-03-29 16:15:21 +0900

    채팅방 ui 인플루언서 닉네임 노출(광고주일때만 노출)

--- pages/chats/index.vue
+++ pages/chats/index.vue
@@ -3,7 +3,7 @@
         <div class="route-wrap" v-if="chat" >
             <div class="container3">
                 <div class="toggle-box">
-                    <p class="title">[{{chat.campaign.title_company}}] {{chat.campaign.title_product}} <br>{{ displayUser ? displayUser.nickname +' 인플루언서' : '' }}</p>
+                    <p class="title">[{{chat.campaign.title_company}}] {{chat.campaign.title_product}} <br>{{ $auth.user.data.type === 'COMPANY' ? displayUser.nickname +' 인플루언서' : '' }}</p>
                     <i class="xi-angle-down active"></i>
                     <i class="xi-close m-mobile" @click.prevent="()=>$router.back()"></i>
                 </div>
