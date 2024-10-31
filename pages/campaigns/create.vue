6f90731f648ee5e2c242be13de34e56ce536a0e7
ShinHyungJune <ssa4141@naver.com>
2024-10-31 20:47:11 +0900

    임시캠페인 불러올 때 accept가 없는 경우 가져오게 처리

--- pages/campaigns/create.vue
+++ pages/campaigns/create.vue
@@ -1124,7 +1124,7 @@ export default {
                     user_id: this.$auth.user.data.id,
                     temp: 1,
                     open: 1,
-                    accept: 0,
+                    accept: "",
                     take: 10,
                 }
             }).then(response => {
